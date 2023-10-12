a68167220aa5d266cc7ae6d05e84f356921280e5
AlessioGesuelliUnicam <105433120+AlessioGesuelliUnicam@users.noreply.github.com>
2023-10-12 16:39:57 +0200

    Initial

---
+++ src/main/java/LoyaltyPlatform/LoyaltyPlatformApplication.java
@@ -0,0 +1,13 @@
+package LoyaltyPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyPlatformApplication.class, args);
+	}
+
+}
