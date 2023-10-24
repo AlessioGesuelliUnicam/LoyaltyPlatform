1a6cb23696ec4030b244cab032c1cd320e253249
BisonniStefano <stefano.bisonni@studenti.unicam.it>
2023-10-24 15:54:21 +0200

    Db getters implemented

--- src/main/java/LoyaltyPlatform/Db/Db.java
+++ src/main/java/LoyaltyPlatform/Db/Db.java
@@ -17,4 +17,67 @@ public class Db {
     private GiftsTable giftsTable;
     private GiftsProgramWalletsTable giftsProgramWalletsTable;
     private LevelsProgramWalletsTable levelsProgramWalletsTable;
+
+    public Db(){
+        clientsTable = new ClientsTable();
+        ownersTable = new OwnersTable();
+        employeesTable = new EmployeesTable();
+        shopsTable = new ShopsTable();
+        coalitionsTable = new CoalitionsTable();
+        levelsProgramsTable = new LevelsProgramsTable();
+        giftsProgramsTable = new GiftsProgramsTable();
+        levelsTable = new LevelsTable();
+        discountsTable = new DiscountsTable();
+        giftsTable = new GiftsTable();
+        giftsProgramWalletsTable = new GiftsProgramWalletsTable();
+        levelsProgramWalletsTable = new LevelsProgramWalletsTable();
+    }
+
+    public ClientsTable getClientsTable() {
+        return clientsTable;
+    }
+
+    public OwnersTable getOwnersTable() {
+        return ownersTable;
+    }
+
+    public EmployeesTable getEmployeesTable() {
+        return employeesTable;
+    }
+
+    public ShopsTable getShopsTable() {
+        return shopsTable;
+    }
+
+    public CoalitionsTable getCoalitionsTable() {
+        return coalitionsTable;
+    }
+
+    public LevelsProgramsTable getLevelsProgramsTable() {
+        return levelsProgramsTable;
+    }
+
+    public GiftsProgramsTable getGiftsProgramsTable() {
+        return giftsProgramsTable;
+    }
+
+    public LevelsTable getLevelsTable() {
+        return levelsTable;
+    }
+
+    public DiscountsTable getDiscountsTable() {
+        return discountsTable;
+    }
+
+    public GiftsTable getGiftsTable() {
+        return giftsTable;
+    }
+
+    public GiftsProgramWalletsTable getGiftsProgramWalletsTable() {
+        return giftsProgramWalletsTable;
+    }
+
+    public LevelsProgramWalletsTable getLevelsProgramWalletsTable() {
+        return levelsProgramWalletsTable;
+    }
 }
