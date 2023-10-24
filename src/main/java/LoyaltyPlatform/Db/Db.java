4d176654d18816c66893f5a2d13eddb07021b1bc
Sofia <sofia.martinelli@studenti.unicam.it>
2023-10-24 13:14:34 +0200

    Setup virtual Db

---
+++ src/main/java/LoyaltyPlatform/Db/Db.java
@@ -0,0 +1,20 @@
+package LoyaltyPlatform.Db;
+
+import LoyaltyPlatform.Transaction.Transaction;
+import LoyaltyPlatform.Wallet.GiftsProgramWallet;
+import LoyaltyPlatform.Wallet.LevelsProgramWallet;
+
+public class Db {
+    private ClientsTable clientsTable;
+    private OwnersTable ownersTable;
+    private EmployeesTable employeesTable;
+    private ShopsTable shopsTable;
+    private CoalitionsTable coalitionsTable;
+    private LevelsProgramsTable levelsProgramsTable;
+    private GiftsProgramsTable giftsProgramsTable;
+    private LevelsTable levelsTable;
+    private DiscountsTable discountsTable;
+    private GiftsTable giftsTable;
+    private GiftsProgramWalletsTable giftsProgramWalletsTable;
+    private LevelsProgramWalletsTable levelsProgramWalletsTable;
+}
