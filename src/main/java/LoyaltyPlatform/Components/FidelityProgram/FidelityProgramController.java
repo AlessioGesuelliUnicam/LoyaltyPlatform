5f0996d217e5d35bb3448059102a8843dc89d822
AlessioGesuelliUnicam <105433120+AlessioGesuelliUnicam@users.noreply.github.com>
2023-10-25 12:45:34 +0200

    delete fidelity program

--- src/main/java/LoyaltyPlatform/Components/FidelityProgram/FidelityProgramController.java
+++ src/main/java/LoyaltyPlatform/Components/FidelityProgram/FidelityProgramController.java
@@ -6,30 +6,54 @@ public class FidelityProgramController {
 
     private final Db db;
 
-    public FidelityProgramController(Db db){
+    public FidelityProgramController(Db db) {
         this.db = db;
     }
 
     /**
      * Creates a new Levels Program
-     * @param multiplier the multiplier used to convert euros into points
+     *
+     * @param multiplier  the multiplier used to convert euros into points
      * @param description the description of the program
      * @return true if the program has been created, false otherwise
      */
-    boolean createLevelsProgram(double multiplier, String description){
+    boolean createLevelsProgram(double multiplier, String description) {
         LevelsProgram levelsProgram = new LevelsProgram(multiplier, description);
         return db.getLevelsProgramsTable().add(levelsProgram);
     }
 
     /**
      * Creates a new Gifts Program
-     * @param multiplier the multiplier used to convert euros into points
+     *
+     * @param multiplier  the multiplier used to convert euros into points
      * @param description the description of the program
      * @return true if the program has been created, false otherwise
      */
-    boolean createGiftsProgram(double multiplier, String description){
+    boolean createGiftsProgram(double multiplier, String description) {
         GiftsProgram giftsProgram = new GiftsProgram(multiplier, description);
         return db.getGiftsProgramsTable().add(giftsProgram);
+    }
+
+    /**
+     * Deletes a Levels Program
+     *
+     * @param id the id of the program to delete
+     * @return true if the program has been deleted, false otherwise
+     */
+    boolean deleteLevelsProgram(int id) {
+        if (id < 0) return false;
+        return db.getLevelsProgramsTable().remove(db.getLevelsProgramsTable().findById(id));
+    }
+
+    /**
+     * Deletes a Gifts Program
+     *
+     * @param id the id of the program to delete
+     * @return true if the program has been deleted, false otherwise
+     */
+    boolean deleteGiftsProgram(int id) {
+        if (id < 0) return false;
+        return db.getGiftsProgramsTable().remove(db.getGiftsProgramsTable().findById(id));
     }
 
 }
