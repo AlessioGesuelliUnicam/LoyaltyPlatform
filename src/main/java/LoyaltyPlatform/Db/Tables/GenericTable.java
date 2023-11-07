10505675d9b93581439f17185bc6116125d9f113
AlessioGesuelliUnicam <105433120+AlessioGesuelliUnicam@users.noreply.github.com>
2023-11-07 11:29:52 +0100

    Adds method getRecordById and interface ObjectWithId

--- src/main/java/LoyaltyPlatform/Db/Tables/GenericTable.java
+++ src/main/java/LoyaltyPlatform/Db/Tables/GenericTable.java
@@ -1,17 +1,20 @@
 package LoyaltyPlatform.Db.Tables;
 
+import LoyaltyPlatform.Utilities.ObjectWithId;
+
 import java.util.HashSet;
 
-public abstract class GenericTable<T> implements Table<T>{
+public abstract class GenericTable<T> implements Table<T> {
 
     HashSet<T> records;
 
-    public GenericTable(){
+    public GenericTable() {
         records = new HashSet<T>();
     }
 
     /**
      * Returns the collection of records
+     *
      * @return the records
      */
     public HashSet<T> getRecords() {
@@ -20,24 +23,41 @@ public abstract class GenericTable<T> implements Table<T>{
 
     /**
      * Adds a record to the collection
+     *
      * @param record the record to add
      * @return true if the element is added, false otherwise
      * @throws NullPointerException if the given Client is null
      */
     public boolean add(T record) {
-        if(record == null) throw new NullPointerException("Field record can't be null");
+        if (record == null) throw new NullPointerException("Field record can't be null");
         return records.add(record);
     }
 
     /**
      * Deletes a record in the collection
+     *
      * @param record the record to delete
      * @return true if the record is deleted, false otherwise
      * @throws NullPointerException if the given record is null
      */
     public boolean delete(T record) {
-        if(record == null) throw new NullPointerException("Field record can't be null");
+        if (record == null) throw new NullPointerException("Field record can't be null");
         return records.remove(record);
+    }
+
+    /**
+     * Returns a record by its id
+     *
+     * @param id the id of the record to find
+     * @return the record if found, null otherwise
+     * @throws IllegalArgumentException if the given id is negative
+     */
+    public T getRecordById(int id) {
+        if (id < 0) throw new IllegalArgumentException("Field id can't be negative");
+        for (T record : records)
+            if (record instanceof ObjectWithId recordWithId)
+                if (recordWithId.getId() == id) return record;
+        return null;
     }
 
 }
---
+++ src/main/java/LoyaltyPlatform/Utilities/ObjectWithId.java
@@ -0,0 +1,10 @@
+package LoyaltyPlatform.Utilities;
+
+public interface ObjectWithId {
+    /**
+     * Return the ID of the Object
+     *
+     * @return the ID
+     */
+    int getId();
+}
